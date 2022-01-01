d8c4ab676f748e823ed127f3bde3085ee9251704
removedporn <86824510+removedporn@users.noreply.github.com>
2022-01-01 19:40:43 +0800

    Delete level3 directory

--- level3/client/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-import { routerReducer } from 'react-router-redux';
-
-import movies from './movies';
-
-export default combineReducers({
-  movies,
-  routing: routerReducer,
-});
